2ca7c469f18fa9c8e6a31803f1619a4bcd83f578
wataken44 <wataken44@hotmail.com>
2012-03-11 20:20:07 +0900

    use yasnippet instead of inc

--- work/inc/macro.cpp
+++
@@ -1,6 +0,0 @@
-
-#define FOR(val, start, end) \
-  for(int val = static_cast<int>(start); val < static_cast<int>(end); ++val)
-#define FORID(val, cont) \
-  for(int val = 0; val < static_cast<int>(cont.size()); ++val)
-
